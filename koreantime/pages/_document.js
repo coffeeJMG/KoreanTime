6e582066a68dada99a699246a8273dba5cd9e4ab
정민규 <jmg2165@naver.com>
2023-09-14 16:40:00 +0900

    불필요한 코드제거

--- koreantime/pages/_document.js
+++ koreantime/pages/_document.js
@@ -7,14 +7,6 @@ export default function Document() {
             <body>
                 <Main />
                 <NextScript />
-                <script
-                    src="//dapi.kakao.com/v2/maps/sdk.js?appkey=9b50e09b61345369e8dbd464a6a583e4&libraries=services,clusterer&autoload=false"
-                    strategy="beforeInteractive"
-                />
-                <script
-                    type="text/javascript"
-                    src="https://developers.kakao.com/sdk/js/kakao.min.js"
-                ></script>
             </body>
             <Main />
             <NextScript />
