58416c6f06b23914f4e6875dafa509f4a7bb9681
정민규 <jmg2165@naver.com>
2023-09-15 11:21:24 +0900

    login 파일 파일명 변경

--- koreantime/app/page.tsx
+++ koreantime/app/page.tsx
@@ -1,7 +1,12 @@
 import getCurrentUser from "./actions/getCurrentUser";
-import LoginForm from "./login/page";
+import LoginForm from "./login/LoginForm";
 
 export default async function Home() {
     const currentUser = await getCurrentUser();
-    return <LoginForm currentUser={currentUser} />;
+
+    return (
+        <>
+            <LoginForm currentUser={currentUser} />
+        </>
+    );
 }
