66e687009b566e8b6b88653e8063209efe686e06
정민규 <jmg2165@naver.com>
2023-09-18 15:55:56 +0900

    Feat : 지도 기능 라이브러리 삭제 후 카카오 공식 맵 사용해서 수정

--- koreantime/app/components/MapLoader.tsx
+++ koreantime/app/components/MapLoader.tsx
@@ -1,11 +1,10 @@
 "use client";
-
-import {
-    Map,
-    MapInfoWindow,
-    MapMarker,
-    useKakaoLoader,
-} from "react-kakao-maps-sdk";
+declare global {
+    interface Window {
+        kakao: any;
+    }
+}
+import React, { useEffect, useState } from "react";
 
 interface MemberLocation {
     memberEmail: string;
@@ -15,9 +14,10 @@ interface MemberLocation {
 }
 
 interface MapLoaderProps {
-    lat?: number;
-    lng?: number;
+    lat: number;
+    lng: number;
     height: string;
+    id: string;
     membersLocation?: MemberLocation[];
 }
 
@@ -26,43 +26,113 @@ const MapLoader: React.FC<MapLoaderProps> = ({
     lng,
     height,
     membersLocation,
+    id,
 }) => {
-    if (typeof lat !== "number" || typeof lng !== "number") {
-        alert("위치를 찾을 수 없습니다.");
-    }
+    const [isLoading, setIsLoading] = useState(false);
 
-    useKakaoLoader({
-        appkey: `${process.env.NEXT_PUBLIC_KAKAO_MAPS_JS_KEY}`,
-    });
+    useEffect(() => {
+        if (lat && lng) {
+            setIsLoading(true);
+        }
+    }, [lat, lng]);
+
+    useEffect(() => {
+        const mapScript = document.createElement("script");
+        mapScript.async = true;
+        mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAPS_JS_KEY}&autoload=false`;
+        document.head.appendChild(mapScript);
+        const onLoadKakaoMap = () => {
+            window.kakao.maps.load(() => {
+                const mapContainer = document.getElementById(`${id}`);
+                const option = {
+                    center: new window.kakao.maps.LatLng(lat, lng),
+                    level: 3,
+                };
+
+                if (!mapContainer) {
+                    return null;
+                }
+                let map = new window.kakao.maps.Map(mapContainer, option);
+
+                if (membersLocation) {
+                    const schedulePosition = new window.kakao.maps.LatLng(
+                        lat,
+                        lng
+                    );
+
+                    const scheduleMarker = new window.kakao.maps.Marker({
+                        position: schedulePosition,
+                    });
+
+                    scheduleMarker.setMap(map);
+
+                    let iwContent = `<div style="padding:5px">도착지</div>`,
+                        iwPosition = new window.kakao.maps.LatLng(lat, lng),
+                        iwRemoveable = true;
+
+                    let infowindow = new window.kakao.maps.InfoWindow({
+                        map: map,
+                        position: iwPosition,
+                        content: iwContent,
+                        removable: iwRemoveable,
+                    });
+
+                    infowindow.open(map, scheduleMarker);
+                    membersLocation.forEach((member) => {
+                        const memberPosition = new window.kakao.maps.LatLng(
+                            member.lat,
+                            member.lng
+                        );
+
+                        const memberMarker = new window.kakao.maps.Marker({
+                            position: memberPosition,
+                        });
+
+                        memberMarker.setMap(map);
+                        let iwContent = `<div style="padding:5px">${member.memberEmail}</div>`,
+                            iwPosition = new window.kakao.maps.LatLng(lat, lng),
+                            iwRemoveable = true;
+
+                        let infowindow = new window.kakao.maps.InfoWindow({
+                            map: map,
+                            position: iwPosition,
+                            content: iwContent,
+                            removable: iwRemoveable,
+                        });
+
+                        infowindow.open(map, memberMarker);
+                    });
+                } else {
+                    let markerPosition = new window.kakao.maps.LatLng(lat, lng);
+                    let marker = new window.kakao.maps.Marker({
+                        position: markerPosition,
+                    });
+                    let iwContent = '<div style="padding:5px">현 위치</div>',
+                        iwPosition = new window.kakao.maps.LatLng(lat, lng),
+                        iwRemoveable = true;
+
+                    let infowindow = new window.kakao.maps.InfoWindow({
+                        map: map,
+                        position: iwPosition,
+                        content: iwContent,
+                        removable: iwRemoveable,
+                    });
+                    marker.setMap(map);
+                    infowindow.open(map, marker);
+                }
+            });
+        };
+
+        mapScript.addEventListener("load", onLoadKakaoMap);
+        return () => {
+            mapScript.removeEventListener("load", onLoadKakaoMap);
+        };
+    }, [lat, lng, membersLocation]);
 
     return (
-        <Map
-            center={{
-                lat: lat || (membersLocation && membersLocation[0]?.lat) || 0,
-                lng: lng || (membersLocation && membersLocation[0]?.lng) || 0,
-            }}
-            style={{
-                width: "100%",
-                height: height,
-            }}
-            level={3}
-        >
-            {membersLocation ? (
-                membersLocation.map((member, index) => (
-                    <div key={index}>
-                        <MapMarker
-                            position={{ lat: member.lat, lng: member.lng }}
-                        >
-                            <div style={{ padding: "5px", color: "#000" }}>
-                                {member.memberEmail}
-                            </div>
-                        </MapMarker>
-                    </div>
-                ))
-            ) : (
-                <MapMarker position={{ lat: lat!, lng: lng! }} />
-            )}
-        </Map>
+        <div>
+            <div id={id} style={{ width: "100%", height: height }}></div>
+        </div>
     );
 };
 
--- koreantime/pages/_document.js
+++ koreantime/pages/_document.js
@@ -6,10 +6,12 @@ export default function Document() {
             <Head />
             <body>
                 <Main />
-                <NextScript />
+                <NextScript
+                    type="text/javascript"
+                    src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAPS_JS_KEY}&libraries=services,clusterer`}
+                />
             </body>
             <Main />
-            <NextScript />
         </Html>
     );
 }
